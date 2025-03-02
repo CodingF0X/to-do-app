8ef0cfa59be5b53c895e6ce802d064dfe8cbeeb0
CodingF0X <roody9a@gmail.com>
2025-03-02 13:59:16 +0100

    -- Feat_Config- added  prop to Joi file

--- backend/src/config/validation.schema.ts
+++ backend/src/config/validation.schema.ts
@@ -1,6 +1,7 @@
 import * as Joi from 'joi';
 
 export const validationSchema = Joi.object({
+  STAGE: Joi.string().required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().required(),
   DB_USERNAME: Joi.string().required(),
