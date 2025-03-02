ec82354a2f190023235ffe1e92f524da85f73dce
CodingF0X <roody9a@gmail.com>
2025-03-02 13:29:57 +0100

    --feature Config - Deleted the hardcoded DB configs

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TaskModule } from './task/task.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { appDataSource } from './typeorm.config';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { typeOrmConfig } from './config/typeorm.config';
