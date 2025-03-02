4a75db5b1358ebbb18daf767a013e3fda2ee5c47
CodingF0X <roody9a@gmail.com>
2025-03-02 13:27:28 +0100

    --Configuration & env setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,10 +3,23 @@ import { TaskModule } from './task/task.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { appDataSource } from './typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { typeOrmConfig } from './config/typeorm.config';
+import databasseConfig from './config/databasse.config';
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      isGlobal: true,
+      ignoreEnvFile: false,
+      load: [databasseConfig],
+    }),
     TaskModule,
-    TypeOrmModule.forRoot(appDataSource.options),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: typeOrmConfig,
+    }),
     AuthModule,
   ],
 })
